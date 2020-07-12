aa145380cdc778a9e3d9ae277c233c7ee61d7da6
dsnezhina <66200617+dsnezhina@users.noreply.github.com>
2020-07-12 12:10:52 +0300

    fix: fix header link styles

--- src/components/link/index.js
+++ src/components/link/index.js
@@ -4,8 +4,8 @@ import styles from './index.module.css';
 
 const Link = ({ title, href }) => {
     return (
-        <li className={styles.listItem}>
-            <a href={href} className={styles.headerLink}>{title}</a>
+        <li className={styles["list-item"]}>
+            <a href={href} className={styles["header-link"]}>{title}</a>
         </li>
     );
 };
